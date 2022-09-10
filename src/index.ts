b32dc3164c07121ddd3bd5cee31bed8ef7549fe1
AleksiPamilo <aleksi.pamilo@live.com>
2022-09-11 01:00:15 +0300

    Create express app, add test for index.ts

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyparser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyparser.json({ limit: "50mb" }));
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.get("/", (_req, res) => {
+    res.status(200).json({
+        message: "Hello World",
+        code: 200,
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+export default app;
---
+++ test/index.test.ts
@@ -0,0 +1,22 @@
+import chai from "chai";
+import chaiHttp from "chai-http";
+import server from "../src/index";
+import "mocha";
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe("Test", () => {
+    it("It should GET {'message': 'Hello World', 'code': 200}", (done) => {
+        chai.request(server)
+            .get("/")
+            .end((_, res) => {
+                chai.should().exist(res.body)
+                res.should.have.status(200);
+                res.body.should.be.a("object");
+                res.body.should.have.property("message").eq("Hello World");
+                res.body.should.have.property("code").eq(200);
+                done();
+            });
+    });
+})
